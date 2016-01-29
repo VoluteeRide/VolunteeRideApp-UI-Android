59e6ee4222ed619e546b2f572cc631a397737015
ayazlakdawala <ayazlakdawala@gmail.com>
2016-01-29 18:21:28 -0500

    #4 Initial check in for ride operation buttons added to ride details screen

---
+++ VolunteeRide/app/src/main/java/com/volunteeride/dto/RideOperationEnum.java
@@ -0,0 +1,12 @@
+package com.volunteeride.dto;
+
+/**
+ * Created by ayazlakdawala on 1/29/2016.
+ */
+public enum RideOperationEnum {
+    REQUEST,
+    ACKNOWLEDGE,
+    CANCEL,
+    ACCEPT,
+    ARCHIVE;
+}
--- VolunteeRide/app/src/main/java/com/volunteeride/volunteeride/RideDetailsActivity.java
+++ VolunteeRide/app/src/main/java/com/volunteeride/volunteeride/RideDetailsActivity.java
@@ -3,12 +3,16 @@ package com.volunteeride.volunteeride;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.volunteeride.dto.Ride;
+import com.volunteeride.dto.RideOperationEnum;
 
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.springframework.util.CollectionUtils;
 
 public class RideDetailsActivity extends AppCompatActivity {
 
@@ -16,9 +20,12 @@ public class RideDetailsActivity extends AppCompatActivity {
 
     private Ride ride;
 
-    private TextView status;
-    private TextView pickUpTime;
-    private TextView pickUpLocation;
+    private TextView statusTextView;
+    private TextView pickUpTimeTextView;
+    private TextView pickUpLocationTextView;
+    private Button cancelRideBttn;
+    private Button acceptRideBttn;
+    private Button acknowledgeRideBttn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +36,59 @@ public class RideDetailsActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-        status = (TextView) findViewById(R.id.textStatus);
-        pickUpTime = (TextView) findViewById(R.id.textPckUpTime);
-        pickUpLocation = (TextView) findViewById(R.id.textPckUpLoc);
+        statusTextView = (TextView) findViewById(R.id.textStatus);
+        pickUpTimeTextView = (TextView) findViewById(R.id.textPckUpTime);
+        pickUpLocationTextView = (TextView) findViewById(R.id.textPckUpLoc);
 
         ride = (Ride) bundle.getSerializable("clickedRide");
-        status.setText(ride.getStatus());
-        pickUpLocation.setText(ride.getPickupLoc().getLocationName());
-        pickUpTime.setText(fmt.print(ride.getPickupTime()));
+        statusTextView.setText(ride.getStatus());
+        pickUpLocationTextView.setText(ride.getPickupLoc().getLocationName());
+        pickUpTimeTextView.setText(fmt.print(ride.getPickupTime()));
+
+        this.initializeRideOperationButtons();
+
+        if(!CollectionUtils.isEmpty(ride.getNextRideUserOperations())){
+            for(String rideOperation : ride.getNextRideUserOperations()){
+
+                RideOperationEnum rideOperationEnum = null;
+                try{
+                    rideOperationEnum = RideOperationEnum.valueOf(rideOperation);
+                }catch(IllegalArgumentException iae){
+                    System.out.println("Exception Occurred While retrieving  ");
+                    //TODO Ayaz Log the exception
+                }
+
+                if(rideOperationEnum != null){
+                    switch (rideOperationEnum){
+                        case CANCEL:
+                            cancelRideBttn.setVisibility(View.VISIBLE);
+                            break;
+                        case ACCEPT:
+                            acceptRideBttn.setVisibility(View.VISIBLE);
+                            break;
+                        case ACKNOWLEDGE:
+                            acknowledgeRideBttn.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                }
+
+
+            }
+        }
+
+
+
+    }
+
+    private void initializeRideOperationButtons() {
+        cancelRideBttn = (Button)findViewById(R.id.bttnCnclRide);
+        cancelRideBttn.setVisibility(View.INVISIBLE);
+
+        acceptRideBttn = (Button)findViewById(R.id.bttnAcptRide);
+        acceptRideBttn.setVisibility(View.INVISIBLE);
+
+        acknowledgeRideBttn = (Button)findViewById(R.id.bttnAcknwldgeRide);
+        acknowledgeRideBttn.setVisibility(View.INVISIBLE);
     }
 
 }
